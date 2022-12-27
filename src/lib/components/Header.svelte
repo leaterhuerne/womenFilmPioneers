c63dfacaeab0eb1e407a97867e83d1d2d66adb3a
Patrick Fock <fock@students.uni-marburg.de>
2022-12-27 14:32:28 +0100

    added english strings to Header.svelte

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+    import { language } from "$lib/stores/language.js";
 
+
+    let title: string = "";
+    
+    onMount(() => {title = $language == "de" ? "Frauen in der frühen Filmgeschichte" : "Women in early Film History"});
 </script>
 
 <div class="w-full p-2 text-center text-paper-200 bg-firebrick-500 ">
-    <h1 class="text-lg">Frauen in der Filmgeschichte</h1>
+    <h1 class="text-lg">{title}</h1>
 </div>
