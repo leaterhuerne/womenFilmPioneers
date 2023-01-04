ed3b57668089daeead1ab96662d06c8b3021a279
leaterhuerne <terhuern@students.uni-marburg.de>
2023-01-04 12:25:13 +0100

    implemented python method for counting professions male/ female

--- src/lib/components/APIDocEntry.svelte
+++ src/lib/components/APIDocEntry.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import { language } from "$lib/stores/language.js";
     import  api  from "$lib/api/api.json";
-    import ApiDocEntry from "$lib/components/APIDocEntry.svelte";
     type apikey = keyof typeof api;
     
 
@@ -11,12 +10,12 @@
 
 <div class="my-2 p-2 border border-warm-gray-400 rounded-lg shadow-lg w-full sm:w-[80%] md:w-2/3">
     <h1 class="mb-2 text-lg font-semibold border-b-2 border-firebrick-500">
-        {$language == "de" ? api[name].title.german : api[name].title.english}
+        {$language === "de" ? api[name].title.german : api[name].title.english}
     </h1>
     <code>
         {api[name].endpoint}
     </code>    
     <p >
-        {$language == "de" ? api[name].description.german : api[name].description.english}
+        {$language === "de" ? api[name].description.german : api[name].description.english}
     </p>
 </div>
--- src/lib/data/processor.py
+++ src/lib/data/processor.py
@@ -1,7 +1,7 @@
 import json
 
 def get_database():
-    with open("databse.json", "r") as file:
+    with open("database.json", "r") as file:
         return json.loads(file.read())
 
 
@@ -9,12 +9,40 @@ def iterate(function):
     database = get_database()
     result = {}
     for entry in database:
-        function(entry, result)
+        function(database, entry, result)
     return result
+
+def countProfessions():
+    print("Loading database...")
+    database = get_database()
+    print("Database loaded. Number of entries: " + str(len(database)))
+    result = {}
+
+    for entry in database:
+        if database[entry]["person"]["Geschlecht"] == "W":
+            gender = "female"
+        elif database[entry]["person"]["Geschlecht"] == "M":
+            gender = "male"
+        else:
+            gender = "unknown"
+
+        for profession in database[entry]["person"]["Taetigkeiten"]:
+            if not profession in result:
+                result[profession] = {
+                    "female": 1 if gender == "female" else 0,
+                    "male": 1 if gender == "male" else 0,
+                    "unknown": 1 if gender == "unknown" else 0
+                }
+            else:
+                result[profession][gender] += 1
+    return json.dumps(result, indent = 4)
+
+
+
 
 
 def main():
-    pass
+    print(countProfessions())
 
 
 if __name__ == "__main__":
