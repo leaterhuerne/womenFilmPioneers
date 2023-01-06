38c5c65229009d743733c0f1cda53fd7ded540f2
Patrick Fock <fock@students.uni-marburg.de>
2023-01-06 21:30:40 +0100

    eliminated errors

--- src/lib/components/APIDocEntry.svelte
+++ src/lib/components/APIDocEntry.svelte
@@ -5,17 +5,18 @@
     
 
     export let name: apikey = "amount-of-women";
+    $: lang = $language === "de" ? "german" : "english";
 
 </script>
 
 <div class="my-2 p-2 border border-warm-gray-400 rounded-lg shadow-lg w-full sm:w-[80%] md:w-2/3">
     <h1 class="mb-2 text-lg font-semibold border-b-2 border-firebrick-500">
-        {$language === "de" ? api[name].title.german : api[name].title.english}
+        {api[name].title[lang]}
     </h1>
     <code>
         {api[name].endpoint}
     </code>    
     <p >
-        {$language === "de" ? api[name].description.german : api[name].description.english}
+        {api[name].description[lang]}
     </p>
 </div>
--- src/lib/components/Language.svelte
+++ src/lib/components/Language.svelte
@@ -4,8 +4,8 @@
 
 <div 
     class="bg-paper-400 text-firebrick-500 font-bold aspect-square h-8 rounded-[50%] cursor-pointer grid place-items-center" 
-    on:click={() => $language = $language == "de" ? "en" : "de"}
-    on:keypress= {() => {}}
+    on:click={() => {$language = $language === "de" ? "en" : "de"; console.log("lang")}}
+    on:keydown={e => console.log(e.keyCode)}
 >
-    <p>{$language == "de" ? "EN" : "DE"}</p>
+    <p>{$language === "de" ? "EN" : "DE"}</p>
 </div>
