20577f039a6683e6cd52572ed524bb0cd94897e7
Patrick Fock <fock@students.uni-marburg.de>
2023-01-07 00:05:56 +0100

    changed design of api documentation page

--- src/lib/components/APIDocEntry.svelte
+++ src/lib/components/APIDocEntry.svelte
@@ -5,18 +5,34 @@
     
 
     export let name: apikey = "amount-of-women";
-    $: lang = $language === "de" ? "german" : "english";
 
 </script>
 
-<div class="my-2 p-2 border border-warm-gray-400 rounded-lg shadow-lg w-full sm:w-[80%] md:w-2/3">
-    <h1 class="mb-2 text-lg font-semibold border-b-2 border-firebrick-500">
-        {api[name].title[lang]}
+<div class="my-2 p-2 border border-warm-gray-200 rounded shadow-xl w-full sm:w-[80%] md:w-2/3">
+    <h1 class="my-2 text-lg font-semibold border-b-2 border-firebrick-500">
+        {api[name].title[$language]}
     </h1>
-    <code>
-        {api[name].endpoint}
-    </code>    
-    <p >
-        {api[name].description[lang]}
+    <div class="my-2 flex justify-between items-center">
+        <code>{api[name].endpoint}</code>
+        <p class="px-1 rounded font-semibold text-paper-100 text-sm bg-firebrick-300">
+            {api[name]["method"]}
+        </p>
+    </div>
+    {#if api[name]["parameters"].length > 0}
+        <h2 class="italic">
+            {$language === "de" ? "Parameter: " : "Parameters: "}
+        </h2>
+        {#each api[name]["parameters"] as parameter}
+            <div class="flex items-center">
+                <code class="mr-2">{parameter["name"]}</code>
+                <p>{parameter[$language]}</p>
+            </div>
+        {/each}
+    {/if}
+    <h2 class="italic mt-2">
+        Response:
+    </h2>
+    <p>
+        {api[name].description[$language]}
     </p>
 </div>
--- src/routes/api/+page.svelte
+++ src/routes/api/+page.svelte
@@ -2,14 +2,25 @@
     import { language } from "$lib/stores/language.js";
     import  api  from "$lib/api/api.json";
     import ApiDocEntry from "$lib/components/APIDocEntry.svelte";
+    import InformationOutline from "$lib/icons/components/InformationOutline.svelte";
 
 
 </script>
 
 <div class="p-2 flex flex-col place-items-center">
-    <h1 class="my-2 text-2xl text-center font-semibold">
-        {$language === "de" ? "API - Liste der verfügbaren Endpoints" : "API - List of Provided Endpoints"}
-    </h1>
+    <div class="flex justify-center items-center">
+        <h1 class="my-2 text-2xl text-center font-semibold">
+            {$language === "de" ? "API - Liste der verfügbaren Endpoints" : "API - List of Provided Endpoints"}
+        </h1>
+        <a
+            href={$language === "de" ? "https://de.wikipedia.org/wiki/Programmierschnittstelle" : "https://en.wikipedia.org/wiki/API"}
+            target="_blank"
+            rel="noreferrer"
+            class="mx-2"
+        >
+            <InformationOutline />
+        </a>
+    </div>
     {#each Object.keys(api) as endpoint}
         <ApiDocEntry name={endpoint} />
     {/each}
