92159cfc5b1359b7cdd68cf5b2d10adc6f6b2cc8
Patrick Fock <fock@students.uni-marburg.de>
2023-01-18 18:11:13 +0100

    endpoint genders-by-year: edited type defintions

--- src/routes/api/genders-by-year/+server.ts
+++ src/routes/api/genders-by-year/+server.ts
@@ -1,9 +1,11 @@
 import data from "$lib/data/genders_by_year.json";
+
 type dataKey = keyof typeof data
 type genderKey = "female" | "male" | "unknown"
+type res = Record<string, number> | number | {"female": number, "male": number, "unknown": number} | typeof data
 
 export function GET({ url }: { url:URL }) {
-    let content = {};
+    let content: res = {};
     const year = url.searchParams.get("year");
     const gender = url.searchParams.get("gender");
     if(year != undefined) {
@@ -14,9 +16,7 @@ export function GET({ url }: { url:URL }) {
         }
     } else if(gender != undefined)  {
         for(const key in data) {
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            content[key] = data[key as dataKey][gender as genderKey]
+            content[key as dataKey] = data[key as dataKey][gender as genderKey]
         }
     } else {
         content = data;
