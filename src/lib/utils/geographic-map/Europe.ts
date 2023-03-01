03019918d3de1b36f68254d14ec2991c4210ab1a
leaterhuerne <terhuern@students.uni-marburg.de>
2023-03-01 16:35:26 +0100

    implemented component of a SVG map of europe

---
+++ src/lib/utils/geographic-map/Europe.ts
@@ -0,0 +1,50 @@
+export { Europe }
+class Europe {
+    // default color for each country is blue
+    russia: string = "blue";
+    germany: string = "blue";
+    unitedKingdom: string = "blue";
+    france: string = "blue";
+    italy: string = "blue";
+    spain: string = "blue";
+    ukraine: string = "blue";
+    poland: string = "blue";
+    romania: string = "blue";
+    netherlands: string = "blue";
+    belgium: string = "blue";
+    czechia: string = "blue";
+    greece: string = "blue";
+    portugal: string = "blue";
+    sweden: string = "blue";
+    hungary: string = "blue";
+    belarus: string = "blue";
+    austria: string = "blue";
+    serbia: string = "blue";
+    switzerland: string = "blue";
+    bulgaria: string = "blue";
+    denmark: string = "blue";
+    finland: string = "blue";
+    slovakia: string = "blue";
+    norway: string = "blue";
+    ireland: string = "blue";
+    croatia: string = "blue";
+    moldova: string = "blue";
+    bosniaAndHerzegovina: string = "blue";
+    albania: string = "blue";
+    lithuania: string = "blue";
+    northMacedonia: string = "blue";
+    slovenia: string = "blue";
+    latvia: string = "blue";
+    kosovo: string = "blue";
+    estonia: string = "blue";
+    montenegro: string = "blue";
+    luxembourg: string = "blue";
+    malta: string = "blue";
+    iceland: string = "blue";
+    andorra: string = "blue";
+    monaco: string = "blue";
+    liechtenstein: string = "blue";
+    sanMarino: string = "blue";
+    turkey: string = "blue";
+
+}
---
+++ src/routes/geographic-map/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import SVGEurope from "$lib/components/geographic-map/SVGEurope.svelte";
+    import {Europe} from "$lib/utils/geographic-map/Europe";
+
+    let countryColors = new Europe();
+
+    let color: string = "green";
+    $: countryColors.sweden = color;
+</script>
+
+<div>
+    <button class="h-20 w-40 bg-slate-300"
+            on:click={() => {
+                color === "blue" ? color = "green" : color = "blue";
+                console.log(color.color)
+            }}>Klick mich</button>
+    <SVGEurope  countries={countryColors} />
+</div>
