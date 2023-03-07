2ed9733f72e43bb7403c56d983e5815aef05caa7
leaterhuerne <terhuern@students.uni-marburg.de>
2023-03-07 18:07:05 +0100

    added listeners to SVGEurope + started HeatMap

---
+++ src/lib/components/geographic-map/HeatMap.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import SVGEurope from "$lib/components/geographic-map/SVGEurope.svelte";
+    type rgb = {red: number, green: number, blue: number};
+    let colorFrom: rgb = {red: 0, green: 0, blue: 255};
+    let colorTo: rgb = {red: 255, green: 0, blue: 0};
+
+    let divBack: rgb = {red: 255, green: 255, blue: 0};
+
+    function mapColor(value: number, lowerBound: number, upperBOund: number): void {
+
+        return;
+    }
+</script>
+
+<div>
+    <input type="range">
+    <div class="w-48 h-8" style="background-color: rgb({divBack.red}, {divBack.green}, {divBack.blue})"></div>
+</div>
--- src/routes/geographic-map/+page.svelte
+++ src/routes/geographic-map/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import SVGEurope from "$lib/components/geographic-map/SVGEurope.svelte";
     import {Europe} from "$lib/utils/geographic-map/Europe";
+    import DuoEurop from "$lib/components/geographic-map/DuoEurop.svelte";
+    import HeatMap from "$lib/components/geographic-map/HeatMap.svelte";
 
     let countryColors = new Europe();
 
@@ -12,7 +14,9 @@
     <button class="h-20 w-40 bg-slate-300"
             on:click={() => {
                 color === "blue" ? color = "green" : color = "blue";
-                console.log(color.color)
+                console.log(color);
             }}>Klick mich</button>
-    <SVGEurope  countries={countryColors} />
+    <!--<SVGEurope  countries={countryColors} /> -->
+    <HeatMap />
+    <!-- <DuoEurop onClick={(id) => {return id}} /> -->
 </div>
