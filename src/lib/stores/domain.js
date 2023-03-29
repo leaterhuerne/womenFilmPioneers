4eaf11428d37d9d635a718adb4f6f9d55782a473
Patrick Fock <fock@students.uni-marburg.de>
2023-03-29 21:52:53 +0200

    implemented database.ts

--- src/lib/stores/domain.js
+++ src/lib/stores/domain.js
@@ -1,3 +1,5 @@
 import { readable } from "svelte/store";
 
-export const domain = readable("http://127.0.0.1:5173");
+const value = "http://127.0.0.1:5173";
+export const domain = readable(value);
+export const domainString = value;
