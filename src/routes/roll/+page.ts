8c55b534b605fd0cee2aab2bbb0e1845290e8657
Patrick Fock <fock@students.uni-marburg.de>
2023-03-29 23:01:24 +0200

    implemented roll/+page.ts

---
+++ src/routes/roll/+page.ts
@@ -0,0 +1,79 @@
+import {domainString} from "$lib/stores/domain";
+type Response = { json: () => any; };
+type gender = "female" | "male" | "unknown";
+interface Consumer<T> {
+    (t: T): void;
+}
+
+type jsumer = (json: JSON) => void
+
+// @ts-ignore
+export function load({fetch}) {
+
+    /**
+     * Returns a JSON containing the requested snapshot of the database.
+     * @param professionList true or undefined
+     * @param locationList true or undefined
+     * @param year number between 1890 and 2021
+     * @param gender string: female, male or unknown
+     * @param location string contained in location list
+     * @param profession string contained in profession list
+     * @return Promise containing a JSON.
+     */
+    const getData = (professionList?: boolean, locationList?: boolean, year?: string | number,
+                            gender?: gender, location?: string, profession?: string,): Promise<JSON> => {
+        const queryValues = {
+            "profession-list": professionList,
+            "location-list": locationList,
+            year: year,
+            gender: gender,
+            location: location,
+            profession: profession
+        }
+        type entry = keyof typeof queryValues;
+        let url: string = domainString + "/api/genders-by-year-profession-location?";
+        for(const entry in queryValues) {
+            if(queryValues[entry as entry] != undefined) {
+                url += entry + "=" + queryValues[entry as entry] + "&";
+            }
+        }
+        return fetch(url).then((response: Response) => response.json());
+    }
+
+    /**
+     * Performs a consumer on a json array containing of all professions in the database.
+     * @param consumer function performed on profession list.
+     */
+    const getProfessionList = (consumer: Consumer<JSON>): void => {
+        getData(true).then(consumer);
+    }
+
+    /**
+     * Performs a consumer on a json array containing of all locations in the database.
+     * @param consumer function performed on location list.
+     */
+    const getLocationList = (consumer: Consumer<JSON>): void => {
+        getData(undefined, true).then(consumer);
+    }
+
+    /**
+     * Performs consumer on JSON containing all years with requested gender, location and profession.
+     * @param location string
+     * @param profession string
+     * @param consumer function to execute on requested data
+     * @return Promise<JSON> JSON containing all years with requested gender, location and profession.
+     */
+    const get = (location: string, profession: string, consumer: Consumer<JSON>): void => {
+        getData(undefined, undefined,undefined, undefined, location, profession).then(consumer);
+    }
+
+    /**
+     * Performs a consumer on the default data.
+     * @param consumer function performed on default data.
+     */
+    const getAllDefault = (consumer: Consumer<JSON>): void => {
+        fetch(domainString + "/api/genders-by-year").then((response: Response) => response.json()).then(consumer);
+    }
+
+    return {getProfessionList,getLocationList, get, getAllDefault};
+}
