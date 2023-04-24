77673add3bcc958c3324b8b9e7267c3618ca7fe6
leaterhuerne <terhuern@students.uni-marburg.de>
2023-04-24 16:59:02 +0200

    added navigation to Header.svelte

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -7,7 +7,7 @@
 <div
         class="
             z-50
-            flex justify-between
+            flex justify-between gap-8 place-items-center
             w-full
             p-2
             text-center
@@ -15,7 +15,17 @@
             bg-firebrick-500 dark:bg-firebrick-1000
         "
 >
-    <span></span>
-    <h1 class="text-lg"><T de="Frauen in der frühen Filmgeschichte" en="Women in Early Film History" /></h1>
+    <a href="/" class="italic font-semibold "><T de="Frauen in der frühen Filmgeschichte" en="Women in Early Film History" /></a>
+    <div class="grow flex justify-end gap-4 place-items-center font-semibold">
+        <a href="/roll" class="hover:underline">
+            <T de="Walze" en="Roll" />
+        </a>
+        <a href="/geographic-map" class="hover:underline">
+            <T de="Europakarte" en="Map of Europe" />
+        </a>
+        <a href="/api" class="hover:underline">
+            API
+        </a>
+    </div>
     <Language />
 </div>
