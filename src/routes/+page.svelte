ce45b4a75d608386812f7fd82a1bdfc336df8221
Patrick Fock <fock@students.uni-marburg.de>
2023-05-01 14:25:59 +0200

    edited start page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,2 @@
-<script lang="ts">
-    import {onMount} from "svelte";
 
-    onMount(() => {
-        console.log("URL: "  + location.hostname + ":" + location.port);
-    })
-</script>
 Hello women's world!
