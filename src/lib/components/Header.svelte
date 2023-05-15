faa1e06136782bdf69d7a391f66c6376c94d2c15
leaterhuerne <terhuern@students.uni-marburg.de>
2023-05-15 17:37:08 +0200

    changed gaps in Header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import { language } from "$lib/stores/language.js";
     import Language from "./Language.svelte";
     import T from "$lib/components/T.svelte";
 </script>
@@ -7,7 +6,7 @@
 <div
         class="
             z-50
-            flex justify-between gap-8 place-items-center
+            flex justify-between gap-4 place-items-center
             w-full
             p-2
             text-center
@@ -15,8 +14,10 @@
             bg-firebrick-500 dark:bg-firebrick-1000
         "
 >
-    <a href="/" class="italic font-semibold "><T de="Frauen in der frühen Filmgeschichte" en="Women in Early Film History" /></a>
-    <div class="grow flex justify-end gap-4 place-items-center font-semibold">
+    <a href="/" class="italic font-semibold ">
+        <T de="Frauen in der frühen Filmgeschichte" en="Women in Early Film History" />
+    </a>
+    <div class="grow flex justify-end gap-3 place-items-center font-semibold">
         <a href="/roll" class="hover:underline">
             <T de="Walze" en="Roll" />
         </a>
